f765991d338ef1d256500caee46fa4406445441f
Andre Leslie <mulandre@Andres-MBP.attlocal.net>
2020-02-05 09:57:32 -0800

    important things

--- src/App.js
+++ src/App.js
@@ -1,8 +1,16 @@
-import React from 'react';
+import React, { useState } from 'react';
+import WeatherCard from './components/WeatherCard';
+import SearchBar from './components/SearchBar';
 
 const App = () => {
+    const [weather, setWeather] = useState({});
+
     return (
-        <h1>Cool Weather App</h1>
+        <div>
+             <h1>Simple Weather App</h1>
+             <SearchBar setWeather={setWeather} />
+             <WeatherCard weather={weather} />
+        </div>
     );  
 }
 
---
+++ src/components/SearchBar.js
@@ -0,0 +1,33 @@
+import React, { useState } from 'react';
+import axios from 'axios';
+
+const SearchBar = (props) => {
+    const [input, setInput] = useState('');
+    const [error, setError] = useState('');
+    const { setWeather } = props;
+
+    const sendSearchEvent = async (e) => {
+        e.preventDefault();
+        let api = `https://cors-anywhere.herokuapp.com/http://api.openweathermap.org/data/2.5/weather?q=${input}&appid=3acc16ffae9e45df92a064e41646355f`; 
+        try { 
+            let response = await axios.get(api)
+            setWeather({...response.data.weather[0], ...response.data.main, name: response.data.name })
+        } catch (err) { 
+            setError('sorry your input is invalid');
+            setTimeout(() => { setError('') }, 3000);
+        }
+    }
+
+    const displayErrorMessage = () => {
+        return error;
+    }
+
+    return (
+        <form onSubmit={sendSearchEvent} >
+            {error.length ? displayErrorMessage() : null}
+            <input type="text" onChange={(e) => setInput(e.target.value)}/> 
+        </form>
+    );
+};
+
+export default SearchBar
